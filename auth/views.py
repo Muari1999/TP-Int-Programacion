de78fb83a0392bed5bcd436f19d5ed5280bd8411
Needfentanyl <theconquer123@gmail.com>
2024-06-12 21:34:45 -0300

    ¿?
    no se

--- auth/views.py
+++ auth/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
+from nasa_image_gallery import home
+# Create your views here.
 
-def home(request):
-    images, favourite_list = []
-    return render(request, 'home.html', {'images': images, 'favourite list': favourite_list})
-    
-def getAllImagesAndFavouriteList(request):
-    images = []
-    favourite_list = []
-
-    return images, favourite_list
-    
